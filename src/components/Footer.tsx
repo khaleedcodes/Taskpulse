d7ac3ce9e827404ffce58be3565ddb76e1c6456b
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-06-10 14:36:32 -0400

    added cstom footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="flex justify-center w-full bg-dark-nav-color">
       <div className=" p-4 w-full justify-start flex">
         <p className="text-white">
-          ✨ by{" "}
+          🧃 by{" "}
           <a
             href="https://khaleed.co/"
             target="_blank"
